9ead51f162eb3adccda4d7324b06fca436815338
Nguyen Van Hung <nguyenvanhung@techmasters-iMac.local>
2017-05-23 16:19:06 +0700

    1st commit

--- drawPedulum/ViewController.swift
+++ drawPedulum/ViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var pendulum: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        drawPendulum()
+        rotate()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func drawPendulum() {
+        
+        pendulum = UIImageView(frame: CGRect (x: 180, y: 180, width: 50, height: 200))
+        pendulum.image = UIImage(named : "pendulum.png")
+        pendulum.layer.anchorPoint = CGPoint(x:0.5, y:0)
+        self.pendulum.transform = CGAffineTransform(rotationAngle: CGFloat (M_PI_4))
+        view.addSubview(pendulum)
     }
-
-
+    
+    func rotate ()
+    {
+        UIView.animate(withDuration: 4, animations:
+        {
+                self.pendulum.transform = CGAffineTransform(rotationAngle: CGFloat (M_PI_4))
+        }) {_ in
+            UIView.animate(withDuration: 4, animations:
+                {
+                    self.pendulum.transform = CGAffineTransform(rotationAngle: CGFloat(-M_PI_4))
+            }){_ in
+                self.rotate()
+            }
+        }
+    }
 }
 
